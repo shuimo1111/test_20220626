b346a94c86b23aa3c7a58e394b0b29baadd71947
shuimo1111 <55583255+shuimo1111@users.noreply.github.com>
2022-06-27 00:34:39 +0800

    删除python1.py
    第一次使用GitHub删除文件，练习一把

--- python.py
+++
@@ -1,9 +0,0 @@
-lst1 = [11, 22, 33, 44]
-lst2 = [11, 22, 33, 44]
-print(lst1 == lst2)
-print(lst1 is lst2)
-print(id(lst1))
-print(id(lst2))
-print(a is not b)
-print(lst1 is not lst2)
-lst1.append(55)
